e74fbd19006afde3ae38d7c45dbe8e4e831abefe
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-11-02 22:13:43 +0100

    feat: support string input

--- browser.js
+++ browser.js
@@ -1,4 +1,4 @@
-import { arr2hex } from './util.js'
+import { arr2hex, text2arr } from './util.js'
 
 const scope = globalThis || window || self
 const crypto = scope.crypto || scope.msCrypto || {}
@@ -7,6 +7,7 @@ const subtle = crypto.subtle || crypto.webkitSubtle
 const hash = subtle ? subtle.digest.bind(subtle, 'sha-1') : () => Promise.reject(new Error('no web crypto support'))
 
 export const sha1 = (data, cb) => {
+  if (typeof data === 'string') data = text2arr(data)
   hash(data).then(ab => cb(arr2hex(new Uint8Array(ab))))
 }
 
